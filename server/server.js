41ae5e68e8c697e0ff7b625481c9aa60c1dad577
Shirin <shirinetezazian@gmail.com>
2021-10-01 17:32:01 -0600

    Add queries for product id and styles

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const PORT = 3000;
 const app = express();
 
 app.get('/products', (req, res) => {
-  const sqlString = 'SELECT * FROM products';
+  const sqlString = 'SELECT * FROM products LIMIT 5';
 
   db.query(sqlString, (err, result) => {
     if (err) {
@@ -15,7 +15,41 @@ app.get('/products', (req, res) => {
     } else {
       res.status(200);
       console.log('Successfully got a list of products!');
-      res.send(`Hello from the server!${result}`);
+      res.json(result);
+    }
+  });
+});
+
+app.get('/products/:product_id', (req, res) => {
+  const sqlString = 'SELECT * FROM products WHERE id = ?';
+  const values = [req.params.product_id];
+
+  db.query(sqlString, values, (err, result) => {
+    if (err) {
+      res.status(500);
+      console.log('error', err);
+      res.send('there was an error getting the product!');
+    } else {
+      res.status(200);
+      console.log(`Successfully got product with id = ${req.params.product_id}`);
+      res.json(result);
+    }
+  });
+});
+
+app.get('/products/:product_id/styles', (req, res) => {
+  const sqlString = 'SELECT * FROM styles WHERE product_id = ?';
+  const values = [req.params.product_id];
+
+  db.query(sqlString, values, (err, result) => {
+    if (err) {
+      res.status(500);
+      console.log('error', err);
+      res.send('there was an error getting the list of styles!');
+    } else {
+      res.status(200);
+      console.log(`Successfully got a list of styles with product id = ${req.params.product_id}`);
+      res.json(result);
     }
   });
 });
