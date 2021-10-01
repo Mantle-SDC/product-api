975a4df87a1cf31393d29cb061a11005f7d4a1a6
Shirin <shirinetezazian@gmail.com>
2021-10-01 17:31:03 -0600

    Update schema to load data into db

--- db/schema.sql
+++ db/schema.sql
@@ -4,12 +4,14 @@ CREATE DATABASE IF NOT EXISTS mantle_product;
 
 USE mantle_product;
 
+SET GLOBAL local_infile=1;
+
 CREATE TABLE products (
   id INT NOT NULL AUTO_INCREMENT,
   name TEXT NOT NULL,
   slogan TEXT NOT NULL,
   description TEXT NOT NULL,
-  category_id INT NOT null,
+  category TEXT NOT null,
   default_price INT NOT NULL,
   PRIMARY KEY (id)
 );
@@ -26,15 +28,9 @@ CREATE TABLE styles (
   id INT NOT NULL AUTO_INCREMENT,
   product_id INT NOT NULL,
   name TEXT NOT NULL,
-  original_price INT NOT NULL,
   sale_price INT,
-  default_style BIT NOT NULL,
-  PRIMARY KEY (id)
-);
-
-CREATE TABLE categories (
-  id INT NOT NULL AUTO_INCREMENT,
-  name TEXT NOT NULL,
+  original_price INT NOT NULL,
+  default_style BIT(1) NOT NULL,
   PRIMARY KEY (id)
 );
 
@@ -49,13 +45,52 @@ CREATE TABLE photos (
 CREATE TABLE skus (
   id INT NOT NULL AUTO_INCREMENT,
   style_id INT NOT NULL,
-  quantity INT NOT NULL,
   size TEXT NOT NULL,
+  quantity INT NOT NULL,
   PRIMARY KEY (id)
 );
 
 ALTER TABLE features ADD FOREIGN KEY (product_id) REFERENCES products (id);
 ALTER TABLE styles ADD FOREIGN KEY (product_id) REFERENCES products (id);
-ALTER TABLE products ADD FOREIGN KEY (category_id) REFERENCES categories (id);
 ALTER TABLE photos ADD FOREIGN KEY (style_id) REFERENCES styles (id);
 ALTER TABLE skus ADD FOREIGN KEY (style_id) REFERENCES styles (id);
+
+-- LOAD DATA LOCAL INFILE './data/product.csv'
+-- -- LOAD DATA LOCAL INFILE 'product_head.csv'
+-- INTO TABLE products
+-- FIELDS TERMINATED BY ','
+-- ENCLOSED BY '"'
+-- LINES TERMINATED BY '\n'
+-- IGNORE 1 LINES;
+
+-- LOAD DATA LOCAL INFILE './data/styles.csv'
+-- -- LOAD DATA LOCAL INFILE 'styles_head.csv'
+-- INTO TABLE styles
+-- FIELDS TERMINATED BY ','
+-- ENCLOSED BY '"'
+-- LINES TERMINATED BY '\n'
+-- IGNORE 1 LINES
+-- (id, product_id, name, sale_price, original_price, @default_style)
+-- SET default_style = CAST(@default_style AS UNSIGNED)
+-- ;
+
+-- LOAD DATA LOCAL INFILE './data/skus.csv'
+-- INTO TABLE skus
+-- FIELDS TERMINATED BY ','
+-- ENCLOSED BY '"'
+-- LINES TERMINATED BY '\n'
+-- IGNORE 1 LINES;
+
+-- LOAD DATA LOCAL INFILE './data/photos.csv'
+-- INTO TABLE photos
+-- FIELDS TERMINATED BY ','
+-- ENCLOSED BY '"'
+-- LINES TERMINATED BY '\n'
+-- IGNORE 1 LINES;
+
+-- LOAD DATA LOCAL INFILE './data/features.csv'
+-- INTO TABLE features
+-- FIELDS TERMINATED BY ','
+-- ENCLOSED BY '"'
+-- LINES TERMINATED BY '\n'
+-- IGNORE 1 LINES;
