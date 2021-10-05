57833b376ad3d6c934df98acba02c7b499bbba76
Shirin <shirinetezazian@gmail.com>
2021-10-05 16:50:37 -0600

    Load all application data for stress testing

--- db/schema.sql
+++ db/schema.sql
@@ -55,42 +55,42 @@ ALTER TABLE styles ADD FOREIGN KEY (product_id) REFERENCES products (id);
 ALTER TABLE photos ADD FOREIGN KEY (style_id) REFERENCES styles (id);
 ALTER TABLE skus ADD FOREIGN KEY (style_id) REFERENCES styles (id);
 
--- LOAD DATA LOCAL INFILE './data/product.csv'
--- -- LOAD DATA LOCAL INFILE 'product_head.csv'
--- INTO TABLE products
--- FIELDS TERMINATED BY ','
--- ENCLOSED BY '"'
--- LINES TERMINATED BY '\n'
--- IGNORE 1 LINES;
+LOAD DATA LOCAL INFILE './data/product.csv'
+-- LOAD DATA LOCAL INFILE 'product_head.csv'
+INTO TABLE products
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;
 
--- LOAD DATA LOCAL INFILE './data/styles.csv'
--- -- LOAD DATA LOCAL INFILE 'styles_head.csv'
--- INTO TABLE styles
--- FIELDS TERMINATED BY ','
--- ENCLOSED BY '"'
--- LINES TERMINATED BY '\n'
--- IGNORE 1 LINES
--- -- (id, product_id, name, sale_price, original_price, @default_style)
--- -- SET default_style = CAST(@default_style AS UNSIGNED)
--- ;
+LOAD DATA LOCAL INFILE './data/features.csv'
+INTO TABLE features
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;
 
--- LOAD DATA LOCAL INFILE './data/skus.csv'
--- INTO TABLE skus
--- FIELDS TERMINATED BY ','
--- ENCLOSED BY '"'
--- LINES TERMINATED BY '\n'
--- IGNORE 1 LINES;
+LOAD DATA LOCAL INFILE './data/styles.csv'
+-- LOAD DATA LOCAL INFILE 'styles_head.csv'
+INTO TABLE styles
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+-- (id, product_id, name, sale_price, original_price, @default_style)
+-- SET default_style = CAST(@default_style AS UNSIGNED)
+;
 
--- LOAD DATA LOCAL INFILE './data/photos.csv'
--- INTO TABLE photos
--- FIELDS TERMINATED BY ','
--- ENCLOSED BY '"'
--- LINES TERMINATED BY '\n'
--- IGNORE 1 LINES;
+LOAD DATA LOCAL INFILE './data/skus.csv'
+INTO TABLE skus
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;
 
--- LOAD DATA LOCAL INFILE './data/features.csv'
--- INTO TABLE features
--- FIELDS TERMINATED BY ','
--- ENCLOSED BY '"'
--- LINES TERMINATED BY '\n'
--- IGNORE 1 LINES;
+LOAD DATA LOCAL INFILE './data/photos.csv'
+INTO TABLE photos
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;
